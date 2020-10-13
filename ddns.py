01c032189001a5971551844acba64a18ee2c3f8c
James Frost <james@frost.cx>
2020-10-13 12:54:42 +0100

    Add files with basic config setup

---
+++ ddns.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+
+import requests
+
+# Loads configuration from ./config.py
+import config as config
+
+print(config.zone)
---
+++ example_config.py
@@ -0,0 +1,6 @@
+# This is a config file for cloudflare ddns.
+# This is a python file, so just replace the values of the variables
+
+subdomain = "www"
+zone = "example.com"
+api_key = "abcdefg"
