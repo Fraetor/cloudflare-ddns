b8f6c3efc46347f6726577e63073070f85e411dd
James Frost <git@frost.cx>
2021-08-05 16:20:18 +0100

    Improve error handling.

--- ddns.py
+++ ddns.py
@@ -3,17 +3,34 @@
 import requests
 from requests.structures import CaseInsensitiveDict
 from time import strftime
+from sys import stderr
 
 # Loads configuration from ./config.py
 import config as config
 
 
 def get_ipv4_address():
-    return requests.get("https://api.ipify.org").text
+    try:
+        ip = requests.get("https://api.ipify.org/", timeout=30).text
+    except TimeoutError as error:
+        print(strftime("%Y-%m-%d %H:%M:%S %Z ") + str(error), file=stderr)
+        exit(1)
+    except requests.exceptions.ConnectionError as error:
+        print(strftime("%Y-%m-%d %H:%M:%S %Z ") + str(error), file=stderr)
+        exit(1)
+    return ip
 
 
 def get_ipv6_address():
-    return requests.get("https://api6.ipify.org").text
+    try:
+        ip = requests.get("https://api6.ipify.org/", timeout=30).text
+    except TimeoutError as error:
+        print(strftime("%Y-%m-%d %H:%M:%S %Z ") + str(error), file=stderr)
+        exit(1)
+    except requests.exceptions.ConnectionError as error:
+        print(strftime("%Y-%m-%d %H:%M:%S %Z ") + str(error), file=stderr)
+        exit(1)
+    return ip
 
 
 def check_record(ip_address, ip_version=4):
@@ -35,7 +52,14 @@ def check_record(ip_address, ip_version=4):
     url = "https://api.cloudflare.com/client/v4/zones/" + config.zone_identifier + \
         "/dns_records/?type=" + record_type + "&name=" + \
         config.subdomain + "." + config.domain
-    dns_json = requests.get(url, headers=headers).json()["result"][0]
+    try:
+        dns_json = requests.get(url, headers=headers, timeout=30).json()["result"][0]
+    except ConnectionError as error:
+        print(error)
+        exit(1)
+    except TimeoutError as error:
+        print(error)
+        exit(1)
     identifier = dns_json["id"]
     dns_address = dns_json["content"]
     if dns_address == ip_address:
@@ -89,3 +113,5 @@ if config.ipv6_enabled:
         if update_record(ident, ipv6_address, 6) == None:
             current_time = strftime("%Y-%m-%d %H:%M:%S %Z")
             print(current_time + " AAAA record updated to " + ipv6_address)
+
+exit(0)
