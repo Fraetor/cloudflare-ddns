cb00167d03cabcc6ee60995c74069b1293805c45
James Frost <git@frost.cx>
2021-08-05 12:53:50 +0100

    Update example config file for rewritten program.

--- example_config.py
+++ example_config.py
@@ -1,7 +1,12 @@
-# This is a config file for cloudflare ddns.
+# This is a config file for cloudflare dDNS.
 # This is a python file, so just replace the values of the variables
 
+# User supplied values
 subdomain = "www"
-zone_ID = "example.com"
-cf_email = "mail@example.com"
-api_key = "abcdefg"
+domain = "example.com"
+zone_identifier = "ab123"
+api_token = "abc123"
+
+# Program configuration
+ipv4_enabled = True
+ipv6_enabled = True
