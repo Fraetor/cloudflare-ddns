4ad50deb56d0ee595908fbdac199c22e951ee59d
James Frost <git@frost.cx>
2021-08-06 23:56:00 +0100

    Add directory schanging to script.

--- sync.sh
+++ sync.sh
@@ -1,4 +1,8 @@
 #! /usr/bin/env bash
 # Requires bash 4 or greater!
 
+# Change working directory to script's location.
+cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null 
+
+# Run DDNS updater.
 python3 ddns.py |& tee -a $1
